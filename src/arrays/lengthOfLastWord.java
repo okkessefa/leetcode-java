073968740c0cbf56973316a0cf7cb82277101ef3
okkessefa <oksekesfackn53@gmail.com>
2025-09-08 22:23:27 +0300

    file directory changes and LeetCode 58st problem solution in 0ms runtime

---
+++ src/arrays/lengthOfLastWord.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int i = s.length() -1;
+        
+        while(i>= 0 && s.charAt(i) == ' '){
+            i--;
+        }
+        if(i<0) return 0;
+
+        int len = 0;
+        while(i>= 0 && s.charAt(i) != ' '){
+            len++;
+            i--;
+        }
+        return len; 
+    }
+}
