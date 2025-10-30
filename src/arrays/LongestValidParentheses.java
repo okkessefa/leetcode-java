775f6296f47fa5a3c7a19d9a5fa38df8c92df69c
okkessefa <oksekesfackn53@gmail.com>
2025-10-30 22:21:04 +0300

    LeetCode 32. hard level problem solution with 5ms runtime

---
+++ src/arrays/LongestValidParentheses.java
@@ -0,0 +1,27 @@
+import java.util.Deque;
+import java.util.ArrayDeque;
+class Solution {
+    public int longestValidParentheses(String s) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int maxLen = 0;
+        stack.push(-1);
+        
+        for(int i =0; i<s.length(); i++){
+            char temp = s.charAt(i);                
+            
+            if(temp == '('){
+                stack.push(i);
+            }
+            else{
+                stack.pop();
+                if(stack.isEmpty()){
+                    stack.push(i);
+                }else{
+                    maxLen= Math.max(maxLen, i-stack.peek());
+                }
+            }
+        }
+
+        return maxLen;
+    }
+}
